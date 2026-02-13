e6564bf4b8fd83c0e2574df47b23bfc9a0e9209e
joaovalcazara <j.francisco.vp@hotmail.com>
2026-02-13 14:05:50 -0300

    feat: configure Vuetify locale settings for Portuguese language support

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,9 +1,15 @@
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
+import { pt } from 'vuetify/locale'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
 export default createVuetify({
   components,
   directives,
+  locale: {
+    locale: 'pt',
+    fallback: 'en',
+    messages: { pt }
+  }
 })
